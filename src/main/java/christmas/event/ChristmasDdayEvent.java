c35a8961e41b9bc9cb7a5de91b17525bec36f6a0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-10 05:31:22 +0900

    feat(christmasDdayEvent): 크리스마스 디데이 할인 생성 및 할인 구현 테스트

---
+++ src/main/java/christmas/event/ChristmasDdayEvent.java
@@ -0,0 +1,21 @@
+package christmas.event;
+
+import christmas.Order;
+
+public class ChristmasDdayEvent implements DateEvent {
+    private final int INITIAL_DISCOUNT_AMOUNT = 1_000;
+    private final int DISCOUNT_INTEREST = 100;
+    private final int endedDate = 25;
+
+    @Override
+    public boolean isConditioned(int expectedVisitDate) {
+        return expectedVisitDate <= endedDate;
+    }
+
+    public void apply(Order order) {
+        int discountPeriod = order.getExpectedVisitDate() - startedDate;
+        int discountAmount = INITIAL_DISCOUNT_AMOUNT + discountPeriod * DISCOUNT_INTEREST;
+
+        order.discount(discountAmount);
+    }
+}
---
+++ src/test/java/christmas/ChristmasDdayEventTest.java
@@ -0,0 +1,22 @@
+package christmas;
+
+import static christmas.domain.menu.Menu.TAPAS;
+import static christmas.domain.menu.Menu.ZERO_COKE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.event.ChristmasDdayEvent;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ChristmasDdayEventTest {
+    Order order = new Order(1, List.of(
+            new OrderItem(TAPAS, 1),
+            new OrderItem(ZERO_COKE, 1)));
+
+    @Test
+    void apply() {
+        new ChristmasDdayEvent().apply(order);
+
+        assertThat(order.getBeforeDiscountPrice()).isEqualTo(ZERO_COKE.getPrice() + TAPAS.getPrice() - 1000);
+    }
+}
