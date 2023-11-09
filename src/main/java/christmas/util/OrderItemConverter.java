3e352caa53a01f99fc506210a1a57fde0c0e7803
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-10 04:27:15 +0900

    feat(orderItemConverter): input값을 파싱한 후 OrderItem으로 변환하는 클래스 생성

---
+++ src/main/java/christmas/util/OrderItemConverter.java
@@ -0,0 +1,28 @@
+package christmas.util;
+
+import static christmas.util.StringParser.splitByMenuAndCount;
+import static christmas.util.StringParser.splitByOrderItem;
+
+import christmas.domain.menu.Menu;
+import christmas.OrderItem;
+import java.util.Arrays;
+import java.util.List;
+
+public class OrderItemConverter {
+    public static List<OrderItem> convert(String input) {
+        String[] orderItems = splitByOrderItem(input);
+
+        return Arrays.stream(orderItems)
+                .map(OrderItemConverter::convertToOrderItem)
+                .toList();
+    }
+
+    private static OrderItem convertToOrderItem(String source) {
+        String[] itemAndCount = splitByMenuAndCount(source);
+
+        Menu item = Menu.valueOf(itemAndCount[0]);
+        int count = Integer.parseInt(itemAndCount[1]);
+
+        return new OrderItem(item, count);
+    }
+}
