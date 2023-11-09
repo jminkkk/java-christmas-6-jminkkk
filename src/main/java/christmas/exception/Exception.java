b59bf962fcc89c5746ef509a81a58b6fabd67aac
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-10 04:30:57 +0900

    feat(exception): 유효하지 않은 주문 에러 exception 이넘에 추가

--- src/main/java/christmas/exception/Exception.java
+++ src/main/java/christmas/exception/Exception.java
@@ -1,6 +1,7 @@
 package christmas.exception;
 
 public enum Exception {
+    INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ONLY_NUMBER_ALLOWED("숫자만 입력 가능합니다."),
     ;
     private final String PREFIX = "[ERROR] ";
