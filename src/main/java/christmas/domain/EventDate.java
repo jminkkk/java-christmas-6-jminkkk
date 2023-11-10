39413a186cf261a6024a013867ec7c231fa15b83
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-11 00:55:16 +0900

    feat(eventDate): 이벤트별 해당 날짜를 나타내는 enum 생성

---
+++ src/main/java/christmas/domain/EventDate.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+import java.util.List;
+
+public enum EventDate {
+    STAR_EVENT_DATE(List.of(3, 10, 17, 24, 25, 31)), // 별
+    WEEKDAY_EVENT_DATE(List.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31)), // 주중
+    WEEKEND_EVENT_DATE(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)), // 주말
+    ;
+    private final List<Integer> conditionDates;
+
+    EventDate(List<Integer> conditionDates) {
+        this.conditionDates = conditionDates;
+    }
+
+    public boolean isConditioned(int expectedVisitDate) {
+        return conditionDates.contains(expectedVisitDate);
+    }
+}
