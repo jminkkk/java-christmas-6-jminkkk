7ba78d67363b77d77e45be5eab196643eeb2f096
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-13 01:05:30 +0900

    feat(inputView): 주문 메뉴 입력 시 , 또는 -가 아닌 다른 문자가 구분자로 사용된 경우

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,10 +1,13 @@
 package christmas.view;
 
 import static christmas.exception.Exception.INVALID_DATE;
+import static christmas.exception.Exception.INVALID_ORDER;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputView {
+    private static final String regex = ".*[가-힣,-]+.*";
     public static int readVisitDate() {
         try {
             return Integer.parseInt(Console.readLine());
@@ -14,6 +17,14 @@ public class InputView {
     }
 
     public static String readMenuAndCount() {
-        return Console.readLine();
+        String menuAndCount = Console.readLine();
+        validate(menuAndCount);
+        return menuAndCount;
+    }
+
+    public static void validate(String menuAndCount) {
+        if (!Pattern.matches(regex, menuAndCount)) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
     }
 }
