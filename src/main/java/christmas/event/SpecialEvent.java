fcd7a573bce05e26f708bd46833b1856b75fde12
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-12 15:04:47 +0900

    feat(specialEvent): 특별할인 클래스 구현

---
+++ src/main/java/christmas/event/SpecialEvent.java
@@ -0,0 +1,19 @@
+package christmas.event;
+
+import christmas.Order;
+import christmas.domain.EventDate;
+
+public class SpecialEvent implements DateEvent {
+    private final int DISCOUNT_AMOUNT = 1_000;
+
+    @Override
+    public boolean isConditioned(int expectedVisitDate) {
+        return EventDate.STAR_EVENT_DATE.isConditioned(expectedVisitDate);
+    }
+
+    @Override
+    public void apply(Order order) {
+        order.discount(DISCOUNT_AMOUNT);
+        order.addEventHistory(this);
+    }
+}
