0b9df61d917bdd49c9ba750abef83f72b0316865
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-13 13:28:56 +0900

    refactor(orderValidator): Order에서 날짜 검증 메서드 OrderValidator로 이동
    - 음료만 주문하였는지 검증하는 메서드의 예외 CAN_NOT_ORDER_ONLY_DRINK로 변경

--- src/main/java/christmas/OrderValidator.java
+++ src/main/java/christmas/OrderValidator.java
@@ -1,5 +1,7 @@
 package christmas;
 
+import static christmas.exception.Exception.CAN_NOT_ORDER_ONLY_DRINK;
+import static christmas.exception.Exception.INVALID_DATE;
 import static christmas.exception.Exception.INVALID_ORDER;
 import static christmas.exception.Exception.ORDER_ONLY_UNDER_MAX;
 
@@ -8,53 +10,62 @@ import christmas.domain.order.OrderItem;
 import java.util.List;
 
 public class OrderValidator {
+    private static final int MIN_VISIT_DATE = 1;
+    private static final int MAX_VISIT_DATE = 31;
     private static final int MIN_MENU_COUNT = 1;
     private static final int MAX_MENU_COUNT = 20;
 
-    public void validate(List<OrderItem> orderItems) {
-        validateMenuDuplication(orderItems);
-        isUnderThanMin(getTotalMenuCount(orderItems));
-        isOverThanMax(getTotalMenuCount(orderItems));
+    public static void validate(int visitDate) {
+        if (visitDate < MIN_VISIT_DATE || visitDate > MAX_VISIT_DATE) {
+            throw new IllegalArgumentException(INVALID_DATE.getMessage());
+        }
     }
+    public static void validate(List<OrderItem> orderItems) {
+        validateMenuDuplication(orderItems);
+        validateOnlyDrink(orderItems);
 
-    private void isUnderThanMin(int totalMenuCount) {
+        int totalMenuCount = getTotalMenuCount(orderItems);
+        isUnderThanMin(totalMenuCount);
+        isOverThanMax(totalMenuCount);
+    }
+    private static void isUnderThanMin(int totalMenuCount) {
         if (totalMenuCount < MIN_MENU_COUNT) {
             throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
     }
 
-    private void isOverThanMax(int totalMenuCount) {
+    private static void isOverThanMax(int totalMenuCount) {
         if (totalMenuCount > MAX_MENU_COUNT) {
             throw new IllegalArgumentException(ORDER_ONLY_UNDER_MAX.getMessage());
         }
     }
 
-    private int getTotalMenuCount(List<OrderItem> orderItems) {
+    private static int getTotalMenuCount(List<OrderItem> orderItems) {
         return orderItems.stream()
                 .mapToInt(OrderItem::getQuantity)
                 .sum();
     }
 
-    private void validateMenuDuplication(List<OrderItem> orderItems) {
+    private static void validateMenuDuplication(List<OrderItem> orderItems) {
         if (orderItems.size() != getDistinctedSize(orderItems)) {
             throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
     }
 
-    private int getDistinctedSize(List<OrderItem> orderItems) {
+    private static int getDistinctedSize(List<OrderItem> orderItems) {
         return (int) orderItems.stream()
                 .map(OrderItem::getMenu)
                 .distinct()
                 .count();
     }
 
-    private void validateOnlyDrink(List<OrderItem> orderItems) {
+    private static void validateOnlyDrink(List<OrderItem> orderItems) {
         if (orderItems.size() == getDrinkCount(orderItems)) {
-            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+            throw new IllegalArgumentException(CAN_NOT_ORDER_ONLY_DRINK.getMessage());
         }
     }
 
-    private int getDrinkCount(List<OrderItem> orderItems) {
+    private static int getDrinkCount(List<OrderItem> orderItems) {
         return (int) orderItems.stream()
                 .map(OrderItem::getMenu)
                 .filter(menu -> menu.isSameCategory(MenuCategory.DRINK))
--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -3,14 +3,13 @@ package christmas.domain.order;
 
 import static christmas.exception.Exception.INVALID_DATE;
 
+import christmas.OrderValidator;
 import christmas.domain.event.DecemberEvent;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Order {
-    private static final int MIN_VISIT_DATE = 1;
-    private static final int MAX_VISIT_DATE = 31;
     private final int expectedVisitDate;
     private final List<OrderItem> orderItems;
     private final int beforeDiscountPrice;
@@ -18,10 +17,12 @@ public class Order {
     private Map<DecemberEvent, Integer> discountHistory;
 
     public Order(int visitDate, List<OrderItem> items) {
-        validate(visitDate);
+        OrderValidator.validate(visitDate);
         this.expectedVisitDate = visitDate;
 
+        OrderValidator.validate(items);
         this.orderItems = items;
+
         beforeDiscountPrice = calculateBeforeDiscountPrice();
         this.discountHistory = new HashMap<>();
     }
@@ -34,21 +35,16 @@ public class Order {
         return orderItems;
     }
 
-    public static void validate(int visitDate) {
-        if (visitDate < MIN_VISIT_DATE || visitDate > MAX_VISIT_DATE) {
-            throw new IllegalArgumentException(INVALID_DATE.getMessage());
-        }
-    }
-
     private int calculateBeforeDiscountPrice() {
         return orderItems.stream()
-            .mapToInt(OrderItem::getTotalPrice)
-            .sum();
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 
     public int getBeforeDiscountPrice() {
         return beforeDiscountPrice;
     }
+
     public void addEventHistory(DecemberEvent event, int discountAmount) {
         discountHistory.put(event, discountAmount);
     }
