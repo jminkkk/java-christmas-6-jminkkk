e5f19a4ed5a835ecf28815b2dd22a81097251fae
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-14 20:56:39 +0900

    feat(badge): 뱃지 이넘 구현

---
+++ src/main/java/christmas/domain/benefit/Badge.java
@@ -0,0 +1,35 @@
+package christmas.domain.benefit;
+
+public enum Badge {
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000),
+    ;
+    private final String name;
+    private final int totalBenefit;
+
+    Badge(String name, int totalBenefit) {
+        this.name = name;
+        this.totalBenefit = totalBenefit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Badge of(int totalBenefitAmount) {
+        if (totalBenefitAmount >= SANTA.totalBenefit) {
+            return SANTA;
+        }
+
+        if (totalBenefitAmount >= TREE.totalBenefit) {
+            return TREE;
+        }
+
+        if (totalBenefitAmount >= STAR.totalBenefit) {
+            return STAR;
+        }
+
+        return null;
+    }
+}
