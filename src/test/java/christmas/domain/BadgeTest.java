ff92f29349dad96213580dd39ec52c82ef49d98f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-15 17:43:12 +0900

    style(badgeTest): 불필요한 import 제거

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -1,8 +1,7 @@
-package christmas.domain.benefit;
+package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.domain.Badge;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
