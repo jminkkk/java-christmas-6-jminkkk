4116ee354d96ce84fad93fab03d70703a2e218f6
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-15 16:09:55 +0900

    test(benefitHistory): 혜택 내역의 총 혜택 금액 및 총 할인 금액을 정상적으로 반환하는지 테스트

---
+++ src/test/java/christmas/domain/benefit/BenefitHistoryTest.java
@@ -0,0 +1,33 @@
+package christmas.domain.benefit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import christmas.domain.event.ChristmasDdayEvent;
+import christmas.domain.event.Event;
+import christmas.domain.event.PresentEvent;
+import christmas.domain.menu.Menu;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BenefitHistoryTest {
+
+    @DisplayName("혜택 내역의 총 혜택 금액 및 총 할인 금액을 정상적으로 반환하는지")
+    @Test
+    void getTotalBenefitAmount() {
+        Map<Event, Benefit> eventAndBenefitAmounts = Map.of(
+                new ChristmasDdayEvent(), Benefit.of(BenefitType.DISCOUNT, 1_000),
+                new PresentEvent(), Benefit.of(BenefitType.PRESENT, 25_000)
+        );
+        List<PresentItem> presentItems = List.of(PresentItem.of(Menu.CHAMPAGNE, 1));
+
+        BenefitHistory benefitHistory = new BenefitHistory(eventAndBenefitAmounts, presentItems);
+
+        assertAll(
+                () -> assertThat(benefitHistory.getTotalBenefitAmount()).isEqualTo(35_000),
+                () -> assertThat(benefitHistory.getTotalDiscountAmount()).isEqualTo(1_000)
+        );
+    }
+}
