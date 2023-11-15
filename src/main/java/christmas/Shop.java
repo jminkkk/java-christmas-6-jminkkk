b947084d24080491d41c53651e830b3a4aaca1e7
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-15 15:41:11 +0900

    feat(shop): 날짜, 메뉴등을 받고 주문, 이벤트 내역을 생성하는 Shop 생성

---
+++ src/main/java/christmas/Shop.java
@@ -0,0 +1,67 @@
+package christmas;
+
+
+import static christmas.global.Comment.INPUT_ORDER_MENU_COMMENT;
+import static christmas.global.Comment.VISIT_DATE_COMMENT;
+import static christmas.global.Comment.WELCOME_COMMENT;
+import static christmas.view.OutputView.println;
+
+import christmas.domain.BenefitHistoryGenerator;
+import christmas.domain.EventPlanner;
+import christmas.domain.benefit.BenefitHistory;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderItem;
+import christmas.util.OrderItemConverter;
+import christmas.validator.OrderItemValidator;
+import christmas.validator.VisitDateValidator;
+import christmas.view.InputView;
+import java.util.List;
+
+public class Shop {
+    private final BenefitHistoryGenerator benefitHistoryGenerator;
+
+    public Shop(BenefitHistoryGenerator benefitHistoryGenerator) {
+        this.benefitHistoryGenerator = benefitHistoryGenerator;
+    }
+
+    public void serveCustomer() {
+        println(WELCOME_COMMENT);
+        int visitDate = askVisitDate();
+        List<OrderItem> orderItems = askOrderMenu();
+
+        Order newOrder = receiveOrder(visitDate, orderItems);
+        BenefitHistory benefitHistory = benefitHistoryGenerator.generateBenefitHistory(newOrder);
+
+        EventPlanner eventPlanner = new EventPlanner(newOrder, benefitHistory);
+        eventPlanner.notifyEventHistory();
+    }
+
+    public int askVisitDate() {
+        println(VISIT_DATE_COMMENT);
+        try {
+            int visitDate = InputView.readVisitDate();
+            VisitDateValidator.validate(visitDate);
+            return visitDate;
+        } catch (IllegalArgumentException e) {
+            println(e);
+            return askVisitDate();
+        }
+    }
+
+    public List<OrderItem> askOrderMenu() {
+        println(INPUT_ORDER_MENU_COMMENT);
+        try {
+            String menuAndCount = InputView.readMenuAndCount();
+            List<OrderItem> orderItems = OrderItemConverter.convert(menuAndCount);
+            OrderItemValidator.validate(orderItems);
+            return orderItems;
+        } catch (IllegalArgumentException e) {
+            println(e);
+            return askOrderMenu();
+        }
+    }
+
+    public Order receiveOrder(int visitDate, List<OrderItem> orderItems) {
+        return Order.of(visitDate, orderItems);
+    }
+}
