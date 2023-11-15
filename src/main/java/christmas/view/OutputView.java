e5d2f9173b5c0004340c3595eb3c14ee0e26ba0b
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-15 15:38:58 +0900

    feat(outputView): printFormatter 제거 및 내부 코드 OutputView로 이동

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,90 +1,62 @@
 package christmas.view;
 
-import static christmas.global.Comment.BEFORE_TOTAL_AMOUNT_COMMENT;
-import static christmas.global.Comment.BENEFIT_HISTORY_COMMENT;
-import static christmas.global.Comment.DECEMBER_EVENT_BADGE_COMMENT;
-import static christmas.global.Comment.EVENT_BENEFITS_COMMENT;
 import static christmas.global.Comment.INPUT_ORDER_MENU_COMMENT;
 import static christmas.global.Comment.NO_VALUE_COMMENT;
-import static christmas.global.Comment.ORDER_MENU_COMMENT;
-import static christmas.global.Comment.PRESENT_MENU_COMMENT;
-import static christmas.global.Comment.TOTAL_BENEFIT_AMOUNT_COMMENT;
-import static christmas.global.Comment.TOTAL_PAYMENT_AMOUNT_COMMENT;
 import static christmas.global.Comment.VISIT_DATE_COMMENT;
 import static christmas.global.Comment.WELCOME_COMMENT;
-import static christmas.global.Comment.DECEMBER;
-import static christmas.util.PrintFormatter.printMoney;
-import static christmas.util.PrintFormatter.printMinusMoney;
-import static christmas.util.PrintFormatter.println;
 
-import christmas.domain.benefit.Badge;
-import christmas.domain.benefit.BenefitHistory;
-import christmas.domain.order.Order;
-import java.util.Optional;
+import christmas.domain.benefit.Benefit;
+import christmas.domain.event.Event;
+import christmas.global.Comment;
+import christmas.util.MoneyFormatter;
+import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
-    public static void printWelcome() {
-        println(WELCOME_COMMENT);
+    private static final String CRLF = "";
+
+    public static void println(String message) {
+        System.out.println(message);
     }
 
-    public static void printVisitDate() {
-        println(VISIT_DATE_COMMENT);
+    public static void println() {
+        println(CRLF);
     }
 
-    public static void printOrderMenu() {
-        println(INPUT_ORDER_MENU_COMMENT);
+    public static void println(IllegalArgumentException exception) {
+        println(exception.getMessage());
     }
 
-    public static void printEventBenefits(Order order, BenefitHistory benefitHistory) {
-        int visitDate = order.getExpectedVisitDate();
-        println(DECEMBER.getMessage() + visitDate + EVENT_BENEFITS_COMMENT.getMessage());
-        println();
-
-        printOrder(order);
-        printBenefits(benefitHistory);
-        printTotalBenefitAmount(benefitHistory.getTotalBenefitAmount());
-        printAmountAfterDiscount(order.getTotalPrice(), benefitHistory);
+    public static void println(Comment comment) {
+        println(comment.getMessage());
     }
 
-    private static void printOrder(Order order) {
-        println(ORDER_MENU_COMMENT);
-        println(order.getOrderItems());
-        println();
-
-        println(BEFORE_TOTAL_AMOUNT_COMMENT);
-        printMoney(order.getTotalPrice());
-        println();
-    }
-
-    private static void printBenefits(BenefitHistory benefitHistory) {
-        println(PRESENT_MENU_COMMENT);
-        println(benefitHistory.getPresentItems());
-        println();
-
-        println(BENEFIT_HISTORY_COMMENT);
-        println(benefitHistory.getEventAndBenefitAmounts());
-        println();
-    }
-
-    private static void printTotalBenefitAmount(int totalBenefitAmount) {
-        println(TOTAL_BENEFIT_AMOUNT_COMMENT);
-        printMinusMoney(totalBenefitAmount);
-        println();
-    }
-
-    public static void printAmountAfterDiscount(int totalPrice, BenefitHistory benefitHistory) {
-        println(TOTAL_PAYMENT_AMOUNT_COMMENT);
-        printMoney(benefitHistory.getAmountAfterDiscount(totalPrice));
-        println();
-    }
-
-    public static void printBadge(Optional<Badge> badge) {
-        println(DECEMBER_EVENT_BADGE_COMMENT);
-        if (badge.isPresent()) {
-            println(badge.get().getName());
+    public static void println(Map<Event, Benefit> eventAndDiscountAmounts) {
+        if (eventAndDiscountAmounts.isEmpty()) {
+            println(NO_VALUE_COMMENT);
             return;
         }
-        println(NO_VALUE_COMMENT);
+
+        eventAndDiscountAmounts.forEach((event, benefits) -> {
+            println(event.getName() + ": " + MoneyFormatter.minusFormat(benefits.getBenefitAmount()));
+        });
+    }
+
+    public static void println(List<?> items) {
+        if (items.isEmpty()) {
+            println(NO_VALUE_COMMENT);
+            return;
+        }
+
+        items.forEach(item -> println(item.toString()));
+    }
+
+    public static void printMoney(int money) {
+        println(MoneyFormatter.format(money));
+    }
+
+    public static void printMinusMoney(int money) {
+        println(MoneyFormatter.minusFormat(money));
     }
 }
