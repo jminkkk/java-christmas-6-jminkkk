c13a6cc56f1407f1e9f3e71d0595e36f0319deae
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-15 16:22:00 +0900

    feat(application): 실행 코드 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,7 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Shop shop = new Shop(new BenefitHistoryGenerator());
+        shop.serveCustomer();
     }
 }
